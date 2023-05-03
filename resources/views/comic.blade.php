d03a9f55ee60ae86c2008c902f41e94d20a88c97
DrKillex <lcollaro1994@gmail.com>
2023-05-03 18:16:47 +0200

    stampa dettagli

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -8,8 +8,47 @@
                     <img src="{{$comic['thumb']}}" alt="" class="img-fluid">
                 </div>
                 <div class="row">
-                    <div class="col-8">
+                    <div class="col-8 pe-5">
                         <h3>{{$comic['title']}}</h3>
+                        <div class="green-banner d-flex justify-content-between">
+                            <p>U.S. Price: {{$comic['price']}}</p>
+                            <div class="d-flex">
+                                <p class="me-3">AVAILABLE</p>
+                                <p>Check availability</p>
+                            </div>
+                        </div>
+                        <p>{{$comic['description']}}</p>
+                    </div>
+                    <div class="col-4">
+                        <h5>ADVERTISEMENT</h5>
+                        <div class="ads">
+                            <img src="{{ Vite::asset('resources/img/adv.jpg') }}" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class="col-12 d-flex">
+                        <div class="talent">
+                            <h4>talent</h4>
+                            <div class="d-flex">
+                                Art by: <p class="names">
+                                    @foreach ($comic['artists'] as $artist)
+                                        {{$artist}}
+                                    @endforeach
+                                </p>
+                            </div>
+                            <div class="d-flex">
+                                Written by: <p class="names">
+                                    @foreach ($comic['writers'] as $writer)
+                                        {{$writer}}
+                                    @endforeach
+                                </p>
+                            </div>
+                        </div>
+                        <div class="spec">
+                            <h4>SPECS</h4>
+                            <div class="d-flex">Series: <p>{{$comic['series']}}</p></div>
+                            <div class="d-flex">U.S. Price: <p>{{$comic['price']}}</p></div>
+                            <div class="d-flex">On Sale Date: <p>{{$comic['sale_date']}}</p></div>
+                        </div>
                     </div>
                 </div>
             </div>
